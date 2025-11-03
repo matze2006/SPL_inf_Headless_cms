f7b9a0cad2ab73adcf679e8ca3ef132578e2c6ed
matze2006 <matthias.h06@gmail.com>
2025-11-03 10:00:25 +0100

    commit_2_CMS

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,10 @@ function App() {
 
   return (
     <>
-     <div className="bg-blue-300 min-h-screen pb-15">
+     <div className="min-h-screen pb-15">
       <h1 className="text-4xl font-bold text-right p-4 mb-8 text-white">HTL Dornbirn 5bWi</h1>
 
-      <div className="grid sm:gap-x-5 md:gap-x-10 lg:gap-x-15 sm:gap-y-5 md:gap-y-7.5 lg:gap-y-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-30">
+      <div className="grid sm:gap-x-5 md:gap-x-10 lg:gap-x-15 sm:gap-y-5 md:gap-y-7.5 lg:gap-y-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-stretch px-30">
         {person.map((p) => (
           <Card
             key={p.id}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,15 +4,17 @@ type Props = {
     person: any
 };
 
-export default function Card({person}: Props) {
+export default function Card({ person }: Props) {
     return (
-        <div className="rounded-xl bg-white w-full font-main">
-            <img className='justify-self-center rounded-xl w-full' src={`https://picsum.photos/id/${person.id}/200`}/>
-            <div className='p-3'>
-                <p className="font-bold pb-2">{person.firstname} {person.lastname}</p>
-                <p>{person.phone}</p>
-                <p>{person.email}</p>
-                <p>{person.birthday}</p>
+        <div className="rounded-xl bg-white w-full h-full font-main flex flex-col">
+            <img className='rounded-xl w-full h-60 object-cover' src={`https://picsum.photos/id/${person.id}/200`} />
+            <div className="flex-1 flex flex-col justify-between p-3">
+                <div>
+                    <p className="font-bold pb-2">{person.firstname} {person.lastname}</p>
+                    <p>{person.phone}</p>
+                    <p>{person.email}</p>
+                    <p>{person.birthday}</p>
+                </div>
             </div>
         </div>
     );
